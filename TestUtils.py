b753fe97121e1ed7e2a11f4e7d18469e84958bf8
RalucaHRotariu <160026760+RalucaHRotariu@users.noreply.github.com>
2024-03-03 18:27:22 +0200

    Update TestUtils.py

--- TestUtils.py
+++ TestUtils.py
@@ -1,10 +1,7 @@
 from selenium import webdriver
-from selenium.common import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
 class TestUtils:
