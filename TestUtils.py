c806ca5183fed728d92db8e3e4f9bef13aae5af9
RalucaHRotariu <160026760+RalucaHRotariu@users.noreply.github.com>
2024-03-03 18:37:46 +0200

    Update TestUtils.py

--- TestUtils.py
+++ TestUtils.py
@@ -1,12 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
 
 
 class TestUtils:
     LANDING_PAGE_URL = "https://www.decathlon.ro"
-    AGREE_TO_COOKIES_BUTTON = (By.ID, "didomi-notice-agree-button")
 
     def setUp(self):
         chrome_options = Options()
